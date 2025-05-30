fe8e7acfe7aa30299a2787b25d6bc16acc9935ee
Robin Dürhager <robin.duerhager@gmail.com>
2025-05-30 15:21:59 +0200

    add tsdoc to auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,7 +2,11 @@ import { Request, Response, NextFunction } from "express"
 import jwt, { JsonWebTokenError, JwtPayload } from 'jsonwebtoken'
 import { JWT_SECRET } from "@/utils/vars";
 
-// Check if the JWT in the header is actually valid
+/**
+ * @description Middleware to check if the user is authenticated by verifying if there is a valid Bearer JWT token in the Authorization header.
+ * @returns A Promise that resolves to the next middleware if the token is valid.
+ * @throws 401 if there is no or an invalid token in the Authorization Header and 500 if the JWT_SECRET environment variable is not set, or if the token is invalid.
+ */
 export const isAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
     if (!JWT_SECRET)
         return res.status(500).send({ error: "Something went wrong" });
